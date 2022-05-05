cba6952b4fddc90814b6b8b2f97981ac8eabd083
VictoryBlue <88573273@qq.com>
2022-05-05 22:45:21 +0800

    basic javascript event

--- 42-flavors/js/script.js
+++ 42-flavors/js/script.js
@@ -1,6 +1,14 @@
 /* Sets a random integer quantity in range [1, 20] for each flavor. */
 function setQuantities() {
-  // TODO
+    let metals = document.getElementsByClassName("meta");
+    metals = Array.from(metals);
+    metals.forEach(element => element.insertBefore(document.createElement("span"),
+      element.children[0]));
+    metals.forEach(element => element.children[0].setAttribute("class","quantity"))
+
+    let quantity_ls = document.getElementsByClassName("quantity");
+    quantity_ls = Array.from(quantity_ls);
+    quantity_ls.forEach(element => element.innerText = Math.floor(Math.random()*19+1));
 }
 
 /* Extracts and returns an array of flavor objects based on data in the DOM. Each
@@ -13,37 +21,85 @@ function setQuantities() {
  * quantity: how many cups of the flavor are available
  */
 function extractFlavors() {
-  // TODO
+  let flavorls = [];
+  let flavor_div_ls = Array.from(document.getElementsByClassName("flavor"));
+  flavor_div_ls.forEach(each => {
+    flavorls.push(
+        {
+          element : each,
+          name : each.querySelector("h2").innerHTML,
+          description : each.querySelector("div[class='description']").querySelector('p').textContent,
+          price : each.querySelector('span[class="price"]').textContent,
+          quantity : each.querySelector('span[class="quantity"]').textContent
+        }
+    );
+  })
+  return flavorls;
 }
 
 /* Calculates and returns the average price of the given set of flavors. The
  * average should be rounded to two decimal places. */
 function calculateAveragePrice(flavors) {
-  // TODO
+  let sum = 0;
+  flavors.forEach(each => {
+    sum += parseFloat(each.price.substring(1));
+  })
+  return (sum/flavors.length).toFixed(2);
 }
 
 /* Finds flavors that have prices below the given threshold. Returns an array
  * of strings, each of the form "[flavor] costs $[price]". There should be
  * one string for each cheap flavor. */
 function findCheapFlavors(flavors, threshold) {
-  // TODO
+    const flavorUnderThreshold = flavors.filter(element =>
+        parseFloat(element.price.substring(1)) < threshold
+    );
+    return flavorUnderThreshold.map(flavor => flavor.name + 'costs ' + flavor.price);
 }
 
 /* Populates the select dropdown with options. There should be one option tag
  * for each of the given flavors. */
 function populateOptions(flavors) {
-  // TODO
+  let flavor_selector = document.querySelector("div[id='footer'] select[name='flavor']");
+  flavors.forEach((each)=>{
+      let option = document.createElement("option");
+      option.innerHTML = each["name"]; //todo innerhtml innertext textcontent
+      option.setAttribute('value',each["name"]);
+      flavor_selector.appendChild(option);
+  })
+
 }
 
 /* Processes orders for the given set of flavors. When a valid order is made,
  * decrements the quantity of the associated flavor. */
 function processOrders(flavors) {
-  // TODO
+    //let flavor_chose = document.querySelector("div[id='footer'] select[name='flavor']").value;
+   // let order_amount = document.querySelector("div[id='footer'] input[type='text']").value;
+
+    let order_button = document.querySelector("input[value='Finalize order →']");
+
+    order_button.addEventListener('click', function (event) {
+        event.preventDefault();
+        let flavor_chose = document.querySelector("div[id='footer'] select[name='flavor']").value;
+        let order_amount = document.querySelector("div[id='footer'] input[type='text']").value;
+        flavors.forEach(each => {
+                    let to_subtract = each.element;
+                    let remain = to_subtract.querySelector("span[class='quantity']");
+                    if (each.name == flavor_chose && parseInt(remain.textContent) >= order_amount) {
+                        remain.textContent = (parseInt(remain.textContent) - order_amount).toString();
+                    }
+                })
+        }
+    )
 }
 
 /* Highlights flavors when clicked to make a simple favoriting system. */
 function highlightFlavors(flavors) {
-  // TODO
+    flavors.forEach(each => {
+        each.element.addEventListener('click',() => {
+              each.element.classList.toggle("highlighted");
+        })
+  })
 }
 
 
@@ -74,3 +130,6 @@ console.log('Cheap flavors:', cheapFlavors)
 populateOptions(flavors)
 processOrders(flavors)
 highlightFlavors(flavors)
+
+
+
